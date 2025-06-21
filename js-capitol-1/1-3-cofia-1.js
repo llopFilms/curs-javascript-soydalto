2c556062544c3630cb0b58c2c4b474f64c75cd6b
llopFilms <llopfilms@gmail.com>
2025-06-21 21:47:31 +0200

    Actualització

--- js-capitol-1/1-3-cofia-1.js
+++ js-capitol-1/1-3-cofia-1.js
@@ -2,14 +2,18 @@ console.log('***** Carregant 1-3-cofia-1.js ******');
 
 console.log(
   `
+Problema 1
+
 3 nois de 23 anys perfectament normals entren a una geladeria a comprar un gelat però hi ha un problema: els preus no estan al costat de cada prestatge amb el gelat respectiu. Volen comprar el gelat més car que puguin amb els diners que tenen, de manera que veiem com podem ajudar-los.
 
 Robert té 1.5€
 Pere té 1.7€
 Cofia té 3€
+2. Si n'hi ha 2 o més amb el mateix preu, mostrar-
 
 Els preus dels gelats són els següents:
 - Bastonet de gelat d'aigua: 0.6€
+2. Si n'hi ha 2 o més amb el mateix preu, mostrar-
 - Bastonet de gelat de crema: 1€
 - Bombó gelat marca Heladix: 1.6€
 - Bombó gelat marca Heladovich: 1.7€
@@ -48,7 +52,7 @@ function definirGelat() {
   let respostaPere = '';
   let respostaCofia = '';
 
-  if (dinersRobert >= 0.6 && dinersRobert < 1) {
+  if (dinersRobert >= 0.6 && dinersRobobjectert < 1) {
     respostaRobert = 'Robert' + missatgeGelatAigua;
     respostaRobert += ' i el canvi es de ' + (dinersRobert - 0.6) + '€';
   } else if (dinersRobert >= 1 && dinersRobert < 1.7) {
@@ -105,4 +109,121 @@ function definirGelat() {
   alert(respostaRobert);
   alert(respostaPere);
   alert(respostaCofia);
+}
+
+console.log(
+  `
+Problema 2
+
+Cofia dona 2.7€ a un que passava per allà. I aquest se'n va a comprar loteria. Cada bitllet costa 0.9€ 
+
+- Si en té prou per comprar un número, el compra
+- Si en té prou per comprar-ne dos, els compra
+- Si en té prou per comprar-ne més, en compra dos i regala el que sobra
+
+Determinar quants en compra i quant sobra
+  `
+);
+
+function calculCompraLoteria() {
+  let costBitlletLoteria = 0.9,
+    pressupostCofia = 2.7;
+
+  let dinersCofia = Number(prompt('Quants diners té Cofia?', pressupostCofia));
+  console.log('dinersCofia -->', dinersCofia);
+
+  let numerodeBitllets = parseInt(dinersCofia / costBitlletLoteria);
+  if (numerodeBitllets > 2) {
+    numerodeBitllets = 2;
+  }
+  console.log('numerodeBitllets -->', numerodeBitllets);
+
+  let canvi = Number((dinersCofia - numerodeBitllets * costBitlletLoteria).toFixed(1));
+  console.log('canvi -->', canvi);
+
+  if (
+    dinersCofia / costBitlletLoteria >= 1 &&
+    dinersCofia / costBitlletLoteria < 2
+  ) {
+    alert(
+      'Cofia compra ' +
+        numerodeBitllets +
+        ' bitllets i el canvi que regala és de ' +
+        canvi +
+        '€'
+    );
+  } else if (
+    dinersCofia / costBitlletLoteria >= 2 &&
+    dinersCofia / costBitlletLoteria < 3
+  ) {
+    alert(
+      'Cofia compra ' +
+        numerodeBitllets +
+        ' bitllets i el canvi que regala és de ' +
+        canvi +
+        '€'
+    );
+  } else {
+    alert(
+      'Cofia compra ' +
+        numerodeBitllets +
+        ' bitllets i el canvi que regala és de ' +
+        canvi +
+        '€'
+    );
+  }
+}
+
+console.log(
+  `
+Problema 3
+
+Hi ha 3 sospitosos d'haver robat un gelat a Cofia és un d'ells per suposat. Crear un programa per fer la comprovació del detector de mentides.
+
+- Preuntar si el sospitós o Cofia han de passar pel detector de mentides
+- Si la resposta és 'Sí', demanar si ha robat el gelat al sospitós:
+  - Si diu que 'Sí', mostrar 'El sospitós ha robat el gelat'
+  - Si diu que 'No', mostrar 'El sospitós no ha robat el gelat'
+  - Si diu qualsevol altra cosa,mostrar 'Repetir la pregunta al sospitós'
+  `);
+
+function detectordeMentides() {
+
+  let preguntaSospitos1 = prompt('El sospitós 1 ha de passar pel detector de mentides? S/N', 'S');
+  let preguntaSospitos2 = prompt('El sospitós 2 ha de passar pel detector de mentides? S/N', 'S');
+  let preguntaCofia = prompt('En Cofia ha de passar pel detector de mentides?S/N', 'S');
+
+  if (preguntaSospitos1 == 'S') {
+    let detectorMentidesSospitos1 = prompt('El sospitós 1 reconeix que ha robat el gelat? S/N', 'S');
+    if (detectorMentidesSospitos1 == 'S') {
+      alert('El sospitós 1 ha robat el gelat');
+    } else if (detectorMentidesSospitos1 == 'N') {
+      alert('El sospitós 1 no ha robat el gelat');
+    } else {
+      alert('Repetir la pregunta al sospitós 1');
+    }
+  }
+
+  if (preguntaSospitos2 == 'S') {
+    let detectorMentidesSospitos2 = prompt('El sospitós 2 reconeix que ha robat el gelat? S/N', 'S');
+    if (detectorMentidesSospitos2 == 'S') {
+      alert('El sospitós 2 ha robat el gelat');
+    } else if (detectorMentidesSospitos2 == 'N') {
+      alert('El sospitós 2 no ha robat el gelat');
+    } else {
+      alert('Repetir la pregunta al sospitós 2');
+    }
+  }
+
+  if (preguntaCofia == 'S') {
+    let detectorMentidesCofia = prompt('En Cofia reconeix que ha robat el gelat? S/N', 'S');
+    if (detectorMentidesCofia == 'S') {
+      alert('En Cofia ha robat el gelat');
+    } else if (detectorMentidesCofia == 'N') {
+      alert('En Cofia no ha robat el gelat');
+    } else {
+      alert('Repetir la pregunta a en Cofia');
+    }
+  }
+
 }
---
+++ js-capitol-2/2-4-cofia-2.js
@@ -0,0 +1,3 @@
+console.log('***** Carregant 2-4-cofia-2.js ******');
+
+
